f82191c978106e24e6396206dde21c10736ed754
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2022-07-31 02:20:49 +0000

    Bump

--- Gemfile
+++ Gemfile
@@ -3,10 +3,8 @@ source 'https://rubygems.org'
 gem 'middleman'
 gem 'middleman-blog'
 gem 'middleman-compass'
-gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
-gem 'middleman-google-analytics'
 gem 'middleman-livereload'
-gem 'middleman-sprockets', '~> 4.0.0.rc'
+gem 'middleman-sprockets'
 gem 'middleman-syntax'
 
 gem 'activesupport', require: 'active_support/all'
@@ -16,6 +14,8 @@ gem 'nokogiri'
 gem 'pry'
 gem 'redcarpet'
 gem 'slim'
+gem 'sass'
+gem 'sprockets', '< 4' # https://github.com/middleman/middleman-sprockets/issues/132
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-gumby'
--- config.rb
+++ config.rb
@@ -30,12 +30,17 @@ configure :build do
   activate :minify_javascript
 end
 
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.branch = 'master'
-  deploy.build_before = true
-end
-
-activate :google_analytics do |ga|
-  ga.tracking_id = 'UA-48013282-1'
+helpers do
+  def google_analytics_universal_tag
+    <<~TAG
+      <script type="text/javascript">
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-48013282-1', 'auto');
+        ga('send', 'pageview');
+      </script>
+    TAG
+  end
 end
